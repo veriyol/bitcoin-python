c6a3174d844d619cb002d76fe42d3ff7be71f7c9
Aviad Reich <aviad.reich@gmail.com>
2013-04-21 13:07:59 +0300

    add tests for connect_to_remote. SSL is Faulty

--- tests/test.py
+++ tests/test.py
@@ -12,45 +12,56 @@ import bitcoinrpc
 from decimal import Decimal
 
 if __name__ == "__main__":
-    conn = bitcoinrpc.connect_to_local()
-    assert(conn.getinfo().testnet) # don't test on prodnet
 
-    assert(type(conn.getblockcount()) is int)
-    assert(type(conn.getconnectioncount()) is int)
-    assert(type(conn.getdifficulty()) is Decimal)
-    assert(type(conn.getgenerate()) is bool)
-    conn.setgenerate(True)
-    conn.setgenerate(True, 2)
-    conn.setgenerate(False)
-    assert(type(conn.gethashespersec()) is int)
-    account = "testaccount"
-    account2 = "testaccount2"
-    bitcoinaddress = conn.getnewaddress(account)
-    conn.setaccount(bitcoinaddress, account)
-    address = conn.getaccountaddress(account)
-    address2 = conn.getaccountaddress(account2)
-    assert(conn.getaccount(address) == account) 
-    addresses = conn.getaddressesbyaccount(account)
-    assert(address in addresses)
-    #conn.sendtoaddress(bitcoinaddress, amount, comment=None, comment_to=None)
-    conn.getreceivedbyaddress(bitcoinaddress)
-    conn.getreceivedbyaccount(account)
-    conn.listreceivedbyaddress()
-    conn.listreceivedbyaccount()
-    #conn.backupwallet(destination)
-    x = conn.validateaddress(address)
-    assert(x.isvalid == True)
-    x = conn.validateaddress("invalid")
-    assert(x.isvalid == False)
+    from bitcoinrpc.config import read_default_config
+    cfg = read_default_config(None)
+    port = int(cfg.get('rpcport', '18332' if cfg.get('testnet') else '8332'))
+    rpcuser = cfg.get('rpcuser', '')
 
-    for accid in conn.listaccounts(as_dict=True).iterkeys():
-      tx = conn.listtransactions(accid)
-      if len(tx) > 0:
-        txid = tx[0].txid
-        txdata = conn.gettransaction(txid)
-        assert(txdata.txid == tx[0].txid)
+    local_conn = bitcoinrpc.connect_to_local()  # will use read_default_config
+    remote_conn = bitcoinrpc.connect_to_remote(
+            user=rpcuser, password=cfg['rpcpassword'], host='localhost', 
+            port=port, use_https=False)
 
-    assert(type(conn.listunspent()) is list)  # needs better testing
+    for conn in (local_conn, remote_conn):
+        assert(conn.getinfo().testnet) # don't test on prodnet
+
+        assert(type(conn.getblockcount()) is int)
+        assert(type(conn.getconnectioncount()) is int)
+        assert(type(conn.getdifficulty()) is Decimal)
+        assert(type(conn.getgenerate()) is bool)
+        conn.setgenerate(True)
+        conn.setgenerate(True, 2)
+        conn.setgenerate(False)
+        assert(type(conn.gethashespersec()) is int)
+        account = "testaccount"
+        account2 = "testaccount2"
+        bitcoinaddress = conn.getnewaddress(account)
+        conn.setaccount(bitcoinaddress, account)
+        address = conn.getaccountaddress(account)
+        address2 = conn.getaccountaddress(account2)
+        assert(conn.getaccount(address) == account) 
+        addresses = conn.getaddressesbyaccount(account)
+        assert(address in addresses)
+        #conn.sendtoaddress(bitcoinaddress, amount, comment=None, comment_to=None)
+        conn.getreceivedbyaddress(bitcoinaddress)
+        conn.getreceivedbyaccount(account)
+        conn.listreceivedbyaddress()
+        conn.listreceivedbyaccount()
+        #conn.backupwallet(destination)
+        x = conn.validateaddress(address)
+        assert(x.isvalid == True)
+        x = conn.validateaddress("invalid")
+        assert(x.isvalid == False)
+
+        for accid in conn.listaccounts(as_dict=True).iterkeys():
+          tx = conn.listtransactions(accid)
+          if len(tx) > 0:
+            txid = tx[0].txid
+            txdata = conn.gettransaction(txid)
+            assert(txdata.txid == tx[0].txid)
+
+        assert(type(conn.listunspent()) is list)  # needs better testing
 
     info = conn.getinfo()
     print "Blocks: %i" % info.blocks
